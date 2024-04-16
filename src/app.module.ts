6befc56ec417c72e79d2b5c8733cf2c60ccd17f6
Aviral Jain <aviraljain1311@gmail.com>
2024-04-16 14:40:25 +0530

    feat: :sparkles: init throttler + add rate limiting to the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AccountModule } from './modules/account/account.module';
 import { ConfigModule } from '@nestjs/config';
 import { FxRatesLiveModule } from './modules/fx-rates-live/fx-rates-live.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -12,7 +14,18 @@ import { FxRatesLiveModule } from './modules/fx-rates-live/fx-rates-live.module'
     MongooseModule.forRoot(process.env.MONGO_URL),
     AccountModule,
     FxRatesLiveModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 4000,
+        limit: 2,
+      },
+    ]),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
